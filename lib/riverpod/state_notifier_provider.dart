f5f0d7360fb6d4ff1c392402deeef5f9f7b5880a
LeeHwaSeok <sky32133@gmail.com>
2023-01-17 22:57:29 +0900

    01/17 - 대부분의 데이터 주석처리

--- lib/riverpod/state_notifier_provider.dart
+++ lib/riverpod/state_notifier_provider.dart
@@ -1,26 +1,36 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_riverpod/model/shopping_item_model.dart';
 
+//상속한 클래스를 Provider로 상태관리시켜주는거임
 final shoppingListProvider =
+    //제너릭은 < 타겟 class , 타겟 type > => 타겟class 형식입니다
     StateNotifierProvider<ShoppingListNotifier, List<ShoppingItemModel>>((ref) => ShoppingListNotifier());
 
+// StateNotifier를 상속해줘야함 type도 선정해줘야합니다.
 class ShoppingListNotifier extends StateNotifier<List<ShoppingItemModel>> {
-  // 상태 초기화
+
+  //ShoppingListNotifier를 불러서 this : this 같은느낌인거같은데 최초 초기화를 진행해야합니다.
   ShoppingListNotifier()
       : super([
+        //Item형식에 맞춰서 ShoppingList...의 초기값을 생성해주는겁니다 init효과
+        // 강사 : StateNotifier에서는 super이하에 타겟 type과 동일한 형태의 데이터의 state를 정의해줘야합니다
           ShoppingItemModel(name: '김치', hasBought: true, isSpicy: false, quantity: 3),
           ShoppingItemModel(name: '라면', hasBought: true, isSpicy: false, quantity: 3),
           ShoppingItemModel(name: '삼겹살', hasBought: false, isSpicy: true, quantity: 13),
           ShoppingItemModel(name: '카스테라', hasBought: true, isSpicy: true, quantity: 6),
-          ShoppingItemModel(name: '수박', hasBought: false, isSpicy: false, quantity: 8)
+          ShoppingItemModel(name: '수박', hasBought: false, isSpicy: false, quantity: 8),
+          ShoppingItemModel(name: '수작', hasBought: false, isSpicy: false, quantity:2 ),
         ]);
 
+  //toggleHasBought는 name값을 받아와서 해당값만 상태를 변경시킵니다.
   void toggleHasBought({required String name}) {
-    // statenotifier을 상속받으면 state가 생성됨
+    // StateNotifier를 상속받으면 state가 생성됩니다. 생성된 state에는 super이하로 생성된 ShoppingItemModel의 데이터들이 존재합니다.
+    // 강사 : state는 super 이하로 생성한 첫번째 파라미터로 초기화가 됩니다.
     state = state
         .map((e) => e.name == name
             ? ShoppingItemModel(name: e.name, hasBought: !e.hasBought, isSpicy: e.isSpicy, quantity: e.quantity)
             : e)
+    //.toList를 사용하는 것은 바뀐 데이터로 새로운 리스트를 반환하기 위해서 사용합니다.
         .toList();
   }
 }
--- lib/screen/state_notifier_provider_screen.dart
+++ lib/screen/state_notifier_provider_screen.dart
@@ -4,12 +4,16 @@ import 'package:test_riverpod/layout/default_layout.dart';
 import 'package:test_riverpod/model/shopping_item_model.dart';
 import 'package:test_riverpod/riverpod/state_notifier_provider.dart';
 
+//ConsumerWidget : state provider를 사용하려면 필요한 클래스
 class StateNotifierProviderScreen extends ConsumerWidget {
   const StateNotifierProviderScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final List<ShoppingItemModel> state = ref.watch(shoppingListProvider);
+    //변경된 값을 가져올때 List<타겟 타입> state= ref.watch(상태관리 변수)
+    final List<ShoppingItemModel> state = ref.watch(shoppingListProvider)
+    //각각의 state별로 포문돌려서 값 출력
+    // state.forEach((instance) => print(instance.name));
 
     return DefaultLayout(
         body: ListView(
@@ -17,6 +21,8 @@ class StateNotifierProviderScreen extends ConsumerWidget {
               .map((e) => CheckboxListTile(
                     value: e.hasBought,
                     onChanged: (value) {
+                      //함수에서 provider를 사용하려면 ref.read를 사용합니다.
+                      // .nofitier를 사용하면 class의 메소드를 사용가능
                       ref.read(shoppingListProvider.notifier).toggleHasBought(name: e.name);
                     },
                     title: Text(e.name),
